1833ef31e5cf6df34938f6a381f698245b820bac
Autapomorph <Autapomorph@users.noreply.github.com>
2020-07-17 13:54:30 +0300

    feat: add throttler

--- src/middlewares/rateLimit.js
+++ src/middlewares/rateLimit.js
@@ -1,11 +1,39 @@
-const rateLimit = require('telegraf-ratelimit');
+const { telegrafThrottler } = require('telegraf-throttler');
+const Bottleneck = require('bottleneck').default;
 
 const { replyErrorRatelimit } = require('../utils/errors/replyError');
+const logger = require('../utils/logger');
 
-const limitConfig = {
-  window: 300,
-  limit: 1,
-  onLimitExceeded: ctx => replyErrorRatelimit(ctx),
-};
+module.exports = telegrafThrottler({
+  in: {
+    maxConcurrent: 1,
+    minTime: 333,
+    highWater: 10,
+    strategy: Bottleneck.strategy.OVERFLOW,
+  },
+  out: {
+    minTime: 25,
+    reservoir: 30,
+    reservoirRefreshAmount: 30,
+    reservoirRefreshInterval: 1 * 1000,
+  },
+  group: {
+    maxConcurrent: 1,
+    minTime: 333,
+    reservoir: 20,
+    reservoirRefreshAmount: 20,
+    reservoirRefreshInterval: 60 * 1000,
+  },
+  onThrottlerError: async (ctx, next, throttlerName, error) => {
+    if (error instanceof Bottleneck.BottleneckError) {
+      logger.warn(`${throttlerName} | ${error.message}`);
 
-module.exports = rateLimit(limitConfig);
+      // Reply only for incoming requests
+      if (/inbound/i.test(throttlerName)) {
+        return replyErrorRatelimit(ctx);
+      }
+    }
+
+    throw error;
+  },
+});
