6b9f526580ca3d4b9ed402630aee8a2aba2e23a2
Autapomorph <Autapomorph@users.noreply.github.com>
2020-11-27 02:07:19 +0300

    feat: disable auto pack creation

--- src/controllers/stickers/add/index.js
+++ src/controllers/stickers/add/index.js
@@ -1,11 +1,7 @@
-const Extra = require('telegraf/extra');
-
 const Pack = require('../../../models/Pack');
-const { replyErrorAddSticker } = require('./replies');
+const { replySuccess, replyErrorNoSuitablePacks, replyErrorAddSticker } = require('./replies');
 const { getUserFile } = require('../../../utils/stickers/get');
 const addSticker = require('../../../utils/stickers/add');
-const createPack = require('../../../utils/packs/create');
-const generateDefaultPack = require('../../../utils/packs/default');
 const ERROR_TYPES = require('../../../utils/errors/errorTypes');
 const validateError = require('../../../utils/errors/validateErrorType');
 const logger = require('../../../utils/logger');
@@ -27,24 +23,15 @@ module.exports = async ctx => {
     await user.save();
   }
 
-  // If user has no selected pack
-  // Create a new pack using defaults and make it the selected one
+  // If user still has no selected pack
+  // Ask them to create a new one
   if (!user.selectedPack) {
-    const defaultPack = generateDefaultPack(user.id, ctx.botInfo.username, userFile.isAnimated);
-    await createPack(ctx, defaultPack);
-    user.selectedPack = await Pack.create(defaultPack);
-    await user.save();
+    return replyErrorNoSuitablePacks(ctx, userFile.isAnimated);
   }
 
   try {
     const { title, link } = await addSticker(ctx, userFile, user.selectedPack);
-    return await ctx.replyWithHTML(
-      ctx.i18n.t('operation.sticker.add.reply.ok', {
-        title,
-        link,
-      }),
-      { ...Extra.inReplyTo(ctx.message.message_id), allow_sending_without_reply: true },
-    );
+    return replySuccess(ctx, { title, link });
   } catch (error) {
     const { STICKERSET_INVALID, STICKERS_TOO_MUCH, STICKER_INVALID_EMOJIS } = ERROR_TYPES.TELEGRAM;
     if (validateError([STICKERSET_INVALID, STICKERS_TOO_MUCH, STICKER_INVALID_EMOJIS], error)) {
--- src/controllers/stickers/add/replies.js
+++ src/controllers/stickers/add/replies.js
@@ -1,6 +1,24 @@
+const Extra = require('telegraf/extra');
+
 const { replyErrorToMessage, replyErrorTelegram } = require('../../../utils/errors/reply');
 const ERROR_TYPES = require('../../../utils/errors/errorTypes');
 const validateError = require('../../../utils/errors/validateErrorType');
+
+const replySuccess = (ctx, { title, link }) => {
+  return ctx.replyWithHTML(
+    ctx.i18n.t('operation.sticker.add.reply.ok', {
+      title,
+      link,
+    }),
+    { ...Extra.inReplyTo(ctx.message.message_id), allow_sending_without_reply: true },
+  );
+};
+
+const replyErrorNoSuitablePacks = (ctx, isAnimated) => {
+  return ctx.replyWithHTML(
+    ctx.i18n.t('operation.sticker.add.reply.error.no_suitable_packs', { isAnimated }),
+  );
+};
 
 const replyErrorAddSticker = (ctx, error) => {
   const { STICKERSET_INVALID, STICKERS_TOO_MUCH, STICKER_INVALID_EMOJIS } = ERROR_TYPES.TELEGRAM;
@@ -21,5 +39,7 @@ const replyErrorAddSticker = (ctx, error) => {
 };
 
 module.exports = {
+  replySuccess,
+  replyErrorNoSuitablePacks,
   replyErrorAddSticker,
 };
