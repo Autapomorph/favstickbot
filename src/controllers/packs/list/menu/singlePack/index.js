e4bb8622739a78b80b27e2e172dcdabf85ac7bad
Autapomorph <Autapomorph@users.noreply.github.com>
2021-03-02 16:57:32 +0300

    revert: vercel import error

--- src/controllers/packs/list/menu/singlePack/index.js
+++ src/controllers/packs/list/menu/singlePack/index.js
@@ -1,6 +1,6 @@
 const { MenuTemplate } = require('telegraf-inline-menu');
 
-const deleteConfirmMenu = require('./deleteConfirm/index.js');
+const deleteConfirmMenu = require('./deleteConfirm');
 const Pack = require('../../../../../models/Pack');
 const actions = require('../../actions');
 const { getMenuBody } = require('./helpers');
