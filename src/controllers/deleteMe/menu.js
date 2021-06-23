6fbcc63d2d41609cd95c491b06eafc21ad10c1d5
Autapomorph <Autapomorph@users.noreply.github.com>
2021-06-23 11:16:21 +0300

    refactor: shorten action callback data

--- src/controllers/deleteMe/menu.js
+++ src/controllers/deleteMe/menu.js
@@ -5,14 +5,14 @@ const { getMenuBody } = require('./helpers');
 
 const menu = new MenuTemplate(getMenuBody);
 
-menu.interact(ctx => ctx.i18n.t('menu.delete_me.actions.yes'), 'yes', {
+menu.interact(ctx => ctx.i18n.t('menu.delete_me.actions.yes'), 'y', {
   do: async ctx => {
     await actions.eraseUserData(ctx);
     return false;
   },
 });
 
-menu.interact(ctx => ctx.i18n.t('menu.delete_me.actions.no'), 'no', {
+menu.interact(ctx => ctx.i18n.t('menu.delete_me.actions.no'), 'n', {
   do: async ctx => {
     await actions.cancel(ctx);
     return false;
--- src/controllers/packs/list/menu/singlePack/deleteConfirm/index.js
+++ src/controllers/packs/list/menu/singlePack/deleteConfirm/index.js
@@ -6,7 +6,7 @@ const packPostfix = require('../../../../../../utils/packs/postfix');
 
 const menu = new MenuTemplate(getMenuBody);
 
-menu.interact(ctx => ctx.i18n.t('menu.packs_list.single_pack.delete_confirm.actions.yes'), 'yes', {
+menu.interact(ctx => ctx.i18n.t('menu.packs_list.single_pack.delete_confirm.actions.yes'), 'y', {
   do: async ctx => {
     const packId = packPostfix.pad(ctx.match[1], ctx.botInfo.username);
     await actions.delete(ctx, packId);
